bcdd4a1a6d34739f451ea08a8950b70313888357
Mohit Sridhar <mohitgsridhar@gmail.com>
2022-02-28 15:28:47 -0800

    Common Mark Test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -3,6 +3,7 @@ import org.junit.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MarkdownParseTest {
@@ -52,6 +53,32 @@ public class MarkdownParseTest {
         
         assertEquals(List.of("https://something.com","some-page.html"), MarkdownParse.getLinks(contents));
     }
+
+    @Test
+    public void testSnippet1() throws IOException {
+        Path fileName = Path.of("testfiles/snippet-1.md");
+        String contents = Files.readString(fileName);
+
+        assertEquals(List.of("google.com", "google.com", "ucsd.edu"), MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void testSnippet2() throws IOException{
+        Path fileName = Path.of("testfiles/snippet-2.md");
+        String contents = Files.readString(fileName);
+
+        assertEquals(List.of("a.com", "a.com", "example.com"), MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void testSnippet3() throws IOException{
+        Path fileName = Path.of("testfiles/snippet-3.md");
+        String contents = Files.readString(fileName);
+
+        assertEquals(List.of("https://cse.ucsd.edu/"), MarkdownParse.getLinks(contents));
+    }
+     
+    
 
     // @Test
     // public void testGetLinksTestFile3() throws IOException {
--- makefile
+++ makefile
@@ -1,4 +1,6 @@
+run: MarkdownParse.class
+	 javac MarkdownParse.java
+	 java MarkdownParse		
 test: 
-	javac MarkdownParse.java
 	javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
 	java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
